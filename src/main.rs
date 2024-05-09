d0bf29b5b83e5e9fe5cede91143b99b2d947e309
Joshua Willis <joshua.willis@intellitect.com>
2024-05-08 18:01:04 -0700

    Closures

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use clap::{Parser, Subcommand, Args};
+use clap::{Args, Parser, Subcommand};
+
+type Input = f32;
+type Output = Input;
 
 #[derive(Debug, Parser)]
 struct Cli {
     #[command(subcommand)]
-    command: Commands
+    command: Commands,
 }
 
 #[derive(Debug, Subcommand)]
@@ -14,21 +17,34 @@ enum Commands {
 
 #[derive(Debug, Args)]
 struct Arguments {
-    lhs: i32,
-    rhs: i32,
+    lhs: Input,
+    rhs: Input,
 }
 
 fn main() {
     let cli = Cli::parse();
 
-    let answer;
+    let args;
+    let func: fn(Input, Input) -> Output;
     match cli.command {
-        Commands::Add(Arguments { lhs, rhs }) => {
-            answer = lhs + rhs;
+        Commands::Add(a) => {
+            args = a;
+            func = add;
         }
-        Commands::Sub(Arguments { lhs, rhs }) => {
-            answer = lhs - rhs;
+        Commands::Sub(a) => {
+            args = a;
+            func = sub;
         }
     }
+
+    let answer = func(args.lhs, args.rhs);
     println!("Answer: {answer}");
+}
+
+fn add(x: Input, y: Input) -> Output {
+    x + y
+}
+
+fn sub(x: Input, y: Input) -> Output {
+    x - y
 }
